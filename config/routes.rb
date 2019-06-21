747b8da4791b60762247442c19a6734e889def7e
mjakerick <mjakerick@gmail.com>
2019-06-20 22:25:20 -0700

    Started working on routes.rb for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index'
+  get '/users', to: 'users#show'
+  post '/users', to: 'users#create'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#delete'
 end
